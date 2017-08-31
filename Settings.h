abc0bdf4e62c8f4550f0e2a05d3c2c88a576d9c3
Austin <abaty@mit.edu>
2017-08-31 14:10:36 -0400

    small updates

--- Settings.h
+++ Settings.h
@@ -9,18 +9,29 @@
 class Settings{
   public:
 
-  std::string inputFile = "/mnt/hadoop/cms/store/user/luck/2015-Data-promptRECO-photonSkims/pp-photonHLTFilter-v0-HiForest/0.root";
+  std::string ppInputFile = "/mnt/hadoop/cms/store/user/luck/2015-Data-promptRECO-photonSkims/pp-photonHLTFilter-v0-HiForest/0.root";
+  //std::string PbPbInputFile = "/mnt/hadoop/cms/store/user/richard/2015-Data-promptRECO-photonSkims/HIPhoton40AndZ/PbPb-photonHLTFilter-v3/160202_145715/000*/*.root";
+
+  bool isPP = true;
+  int nEvtToProcess = -1;//set to -1 to get full data sample
 
   int jetRadiusTimes10 = 3;
+  static const int nSoftDrops = 5;
+  float subjetDRCut = 0.1;
+  float softDropZ[nSoftDrops] = {0.1,0.2,0.5,0.1,0.2};
+  float softDropBeta[nSoftDrops] = {0,0,1.5,1,1};
 
+  std::string phoTrigger = "HLT_HISinglePhoton40_Eta1p5_v1";
   float phoPtCut = 60;
   float phoEtaCut = 1.44;
   float jetPtCut = 30;
-  float jetEtaCut = 2;
+  float jetEtaCut = 1.6;
+  float dPhiCut = 2.74889357; //7pi/8
 
 
   //for controlling threads
-  
+  std::pair< float, float > photon[2];//stores phi,pt for passing between threads
+  std::vector< std::vector< float > > b2bJets[2];//stores eta,phi,pt of back to back jets for passing between threads
   int nextEvt[2] = {-1,-1};
   bool isQueueFull = false;
   bool isEvtThreadDone = false;
--- run.C
+++ run.C
@@ -9,6 +9,7 @@
 #include "TThread.h"
 //#include "TMutex.h"
 #include "TCondition.h"
+#include "TLorentzVector.h"
 #include <vector>
 #include <string>
 #include <iostream>
@@ -20,7 +21,7 @@ TMutex mtx1;
 
 void *findEvts(void *ptr){
   Settings * s = (Settings*) ptr;
-  TFile * f = TFile::Open(s->inputFile.c_str(),"read");
+  TFile * f = TFile::Open(s->ppInputFile.c_str(),"read");
   TTree * skim = (TTree*)f->Get("skimanalysis/HltTree");
   TTree * photons = (TTree*)f->Get("ggHiNtuplizerGED/EventTree");  
   TTree * evt = (TTree*)f->Get("hiEvtAnalyzer/HiTree");
@@ -64,14 +65,14 @@ void *findEvts(void *ptr){
   photons->SetBranchAddress("phoPhi",&phoPhi);
   photons->SetBranchAddress("phoEta",&phoEta);
   evt->SetBranchAddress("vz",&vz);
-  hlt->SetBranchAddress("HLT_HISinglePhoton50_Eta3p1_v1",&trig);
+  hlt->SetBranchAddress(s->phoTrigger.c_str(),&trig);
   jet->SetBranchAddress("nref",&nref);
   jet->SetBranchAddress("jtpt",&jtpt);
   jet->SetBranchAddress("jtphi",&jtphi);
   jet->SetBranchAddress("jteta",&jteta);
 
 
-  for(int i = 0; i<100000; i++){//i<skim->GetEntries(); i++){
+  for(int i = 0; i< ((s->nEvtToProcess<0)?skim->GetEntries():s->nEvtToProcess); i++){
     if(i%10000 == 0) std::cout << i << "/" << skim->GetEntries() << std::endl;
     hlt->GetEntry(i);
     if(!trig) continue;
@@ -80,25 +81,36 @@ void *findEvts(void *ptr){
     if(TMath::Abs(vz)>15) continue;
 
     photons->GetEntry(i);
-    std::vector< float > goodPhotonPhi;
+    std::vector< std::pair< float, float > > goodPhotons;
     for(unsigned int j = 0; j<phoEt->size(); j++){
       if(phoEt->at(j) < s->phoPtCut) continue;
       if(TMath::Abs(phoEta->at(j)) > s->phoEtaCut) continue;
       if(!(TMath::Abs(seedTime->at(j))<3 && swissCrx->at(j)<0.9 && sigmaIEtaIEta->at(j)>0.002)) continue;//spike rejection
       if(!((ecalR4->at(j)+hcalR4->at(j)+trkR4->at(j)) < 1 && (hOverE->at(j)<0.1))) continue;//isolation
       if(!(sigmaIEtaIEta->at(j)<0.01)) continue;//sigmaietaieta
-      goodPhotonPhi.push_back(phoPhi->at(j));
+      goodPhotons.push_back(std::pair<float, float>(phoPhi->at(j), phoEt->at(j)));
     }
-    if(goodPhotonPhi.size()==0) continue;
+    if(goodPhotons.size()==0) continue;
 
     jet->GetEntry(i);
     bool hasBackToBackJet = false;
+    int photonIndx = -1;
+    std::vector< std::vector< float > > jetVars;
     //caution, assumes jet tree is ordered by pt for speed!
     for(unsigned int j = 0; j<nref; j++){
       if(jtpt[j] < s->jetPtCut) break;
       if(TMath::Abs(jteta[j]) > s->jetEtaCut) continue;
-      for(unsigned int p = 0; p<goodPhotonPhi.size(); p++){
-        if(TMath::ACos(TMath::Cos(goodPhotonPhi.at(p)-jtphi[j])) > 5*TMath::Pi()/6.0) hasBackToBackJet = true; 
+      for(unsigned int p = 0; p<goodPhotons.size(); p++){
+        if(TMath::ACos(TMath::Cos(goodPhotons.at(p).first-jtphi[j])) > s->dPhiCut){
+          hasBackToBackJet = true; 
+          photonIndx = p;
+          std::vector< float > tempJet;
+          tempJet.push_back(jteta[j]);
+          tempJet.push_back(jtphi[j]);
+          tempJet.push_back(jtpt[j]);
+          jetVars.push_back(tempJet);
+          continue;
+        }
       } 
     }
     if(!hasBackToBackJet) continue;
@@ -106,18 +118,23 @@ void *findEvts(void *ptr){
     //put cuts above here
     //controls placing event index into a buffer accesible by the other thread
     mtx1.Lock();
-      while(s->isQueueFull){
-        mtx1.UnLock();
-        sleep(0.1);
-        //std::cout << "Thread 2 waiting" << std::endl;
-        mtx1.Lock();
-      }
-      //std::cout << "placing evt " << i << std::endl;
-      if(s->nextEvt[0] == -1) s->nextEvt[0] = i;
-      else if(s->nextEvt[1] == -1){
-        s->nextEvt[1] = i;
-        s->isQueueFull = true;
-      } 
+    while(s->isQueueFull){
+      mtx1.UnLock();
+      sleep(0.1);
+      //std::cout << "Thread 2 waiting" << std::endl;
+      mtx1.Lock();
+    }
+    //std::cout << "placing evt " << i << std::endl;
+    if(s->nextEvt[0] == -1){
+      s->nextEvt[0] = i;
+      (s->photon)[0] = goodPhotons.at(photonIndx);
+      (s->b2bJets)[0] = jetVars;
+    } else if(s->nextEvt[1] == -1){
+      s->nextEvt[1] = i;
+      (s->photon)[1] = goodPhotons.at(photonIndx);
+      (s->b2bJets)[1] = jetVars;
+      s->isQueueFull = true;
+    } 
     mtx1.UnLock();    
   }
   std::cout << "Thread 1 Done!" << std::endl;
@@ -128,19 +145,25 @@ void *findEvts(void *ptr){
 
 
 void runGrooming(){
+  TH1::SetDefaultSumw2();
   Settings s = Settings();
   std::cout << "Initializing evt search thread" << std::endl;
  
-  //setup
-
-
-
   //thread setup and control
   TThread * thread1 = new TThread("thread1",findEvts, (void*)(&s));
   thread1->Run();
   bool isDone = false;
 
-  TFile * f = TFile::Open(s.inputFile.c_str(),"read");
+  TFile * out = TFile::Open("output.root","recreate");
+  TH1D * xjg_LT[s.nSoftDrops];
+  TH1D * xjg_GT[s.nSoftDrops];
+  for(int i = 0; i<s.nSoftDrops; i++){
+    xjg_LT[i] = new TH1D(Form("xjg_lt_%d",i),Form("xjg_lt_%d",i),20,0,2);
+    xjg_GT[i] = new TH1D(Form("xjg_gt_%d",i),Form("xjg_gt_%d",i),20,0,2);
+  } 
+
+  //loading pfcands
+  TFile * f = TFile::Open(s.ppInputFile.c_str(),"read");
   TTree * pf = (TTree*)f->Get("pfcandAnalyzer/pfTree");
 
   std::vector< int > * id = 0;
@@ -155,33 +178,80 @@ void runGrooming(){
   pf->SetBranchAddress("pfPhi",&phi);
   pf->SetBranchAddress("pfEnergy",&e);
 
+  int nPho = 0;
+  float photonPhi = -1;
+  float photonPt = -1;
+  std::vector<  std::vector< float > > jetVars;//contains a list of jets w/ eta/phi/pt
+
+
   while(true){
     sleep(0.01);
     //std::cout << "Thread 1 waiting" << std::endl;
     mtx1.Lock();
       if(s.nextEvt[0] != -1){
         int thisEvt = s.nextEvt[0];
+        photonPhi = (s.photon)[0].first;
+        photonPt = (s.photon)[0].second;
+        jetVars = (s.b2bJets)[0];
         s.nextEvt[0] = s.nextEvt[1];
-        s.nextEvt[1] = -1;
+        (s.photon)[0] = (s.photon)[1];
+        (s.b2bJets)[0] = (s.b2bJets)[1];
+        s.nextEvt[1] = -1; 
         s.isQueueFull = false;
         if(s.isEvtThreadDone && s.nextEvt[0]==-1) isDone = true;
         mtx1.UnLock();
 
         //analysis code here
+        std::cout << "\nPhoton Pt: " << photonPt << " Photon Phi: " << photonPhi << std::endl;
+
+        //pfcandSetup for fastJet
         pf->GetEntry(thisEvt);
+        nPho++;
         std::vector<PseudoJet> particles;
         for(unsigned int i = 0; i<id->size(); i++){
-          if(TMath::Abs(eta->at(i))>2.9) continue;
-          particles.push_back( PseudoJet(pt->at(i)*TMath::Cos(phi->at(i)),pt->at(i)*TMath::Sin(phi->at(i)),pt->at(i)/(TMath::Tan(2*TMath::ATan(TMath::Exp(-eta->at(i))))),e->at(i)));//FIXME change to TLorentzVector constructor
+          TLorentzVector vec = TLorentzVector();
+          vec.SetPtEtaPhiE(pt->at(i),eta->at(i), phi->at(i), e->at(i));
+          particles.push_back( PseudoJet(vec.Px(),vec.Py(),vec.Pz(), vec.E()) );
         }
 
+        //clustering algorithm
         JetDefinition jet_def(antikt_algorithm, s.jetRadiusTimes10/10.0);
         ClusterSequence cs(particles, jet_def);
-        std::vector<PseudoJet> jets = sorted_by_pt(cs.inclusive_jets(30.0));
-        std::cout <<   "        pt y phi" << std::endl;
-        for (unsigned i = 0; i < jets.size(); i++) {
-          std::cout << "jet " << i << ": "<< jets[i].pt() << " " 
-                   << jets[i].rap() << " " << jets[i].phi() << std::endl;
+        std::vector<PseudoJet> jets = sorted_by_pt(cs.inclusive_jets());
+
+        //matching and SoftDrop
+        std::vector<PseudoJet> matchedJets;
+        softDropGroomer sd[s.nSoftDrops];
+        for(int k = 0; k<s.nSoftDrops; k++) sd[k] = softDropGroomer(s.softDropZ[k], s.softDropBeta[k], s.jetRadiusTimes10/10.0);
+
+        for(unsigned int j = 0; j < jetVars.size(); j++){
+          bool isMatched = false;
+          std::cout << "Forest Jet " << j << " Pt: " << jetVars.at(j).at(2) << " Eta: " << jetVars.at(j).at(0) 
+                  << " Phi: " << jetVars.at(j).at(1) << std::endl;
+
+          for (unsigned int i = 0; i < jets.size(); i++) {
+            if(TMath::Abs(jets[i].eta()-jetVars.at(j).at(0))>0.05) continue;
+            if(TMath::Abs(jets[i].phi_std()-jetVars.at(j).at(1))>0.05) continue;
+            isMatched = true;
+            std::cout << "FJ jet: " << i << ": "<< jets[i].pt() << " " 
+                   << jets[i].eta() << " " << jets[i].phi_std() << std::endl;
+            for(int k = 0; k<s.nSoftDrops; k++){
+              std::vector<PseudoJet> groomedJets = sd[k].doGrooming(jets);
+
+              if(sd[k].getDR12().at(i) <= s.subjetDRCut) xjg_LT[k]->Fill(jetVars.at(j).at(2)/photonPt);
+              else xjg_GT[k]->Fill(jetVars.at(j).at(2)/photonPt);
+            }
+            continue; 
+          }
+          /*if(!isMatched){
+            std::cout << "No Match!!!" << std::endl;
+            std::cout << "Forest jet: " << j << ": "<< jetVars.at(j).at(2) << " "
+                   << jetVars.at(j).at(0) << " " << jetVars.at(j).at(1) << std::endl;
+            for (unsigned int i = 0; i < jets.size(); i++) {
+              std::cout << "FJ jet: " << i << ": "<< jets[i].pt() << " "
+                   << jets[i].eta() << " " << jets[i].phi() << std::endl;
+            }
+          }*/
         }
 
         //std::cout << "done with " << thisEvt << std::endl;
@@ -196,6 +266,12 @@ void runGrooming(){
 
   }
   thread1->Join();
+  for(int i = 0; i<s.nSoftDrops; i++){
+    xjg_LT[i]->Scale(1/(float)nPho);
+    xjg_GT[i]->Scale(1/(float)nPho);
+    xjg_GT[i]->SetLineColor(kRed);
+  } 
+  out->Write();
 }
 
 int main(){
